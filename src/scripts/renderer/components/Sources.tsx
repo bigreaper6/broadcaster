07d4e6f9fafbfbed9ef0824afc1021b5c310a681
Oden <ops991923@gmail.com>
2017-01-18 16:36:27 -0800

    Use list component for sources

--- src/scripts/renderer/components/Sources.tsx
+++ src/scripts/renderer/components/Sources.tsx
@@ -1,59 +1,37 @@
 import * as React from "react";
-import {computed} from "mobx";
 import {observer} from "mobx-react";
 import t from "../../i18n";
-import ScenesStore from "../../stores/scenes";
 import UiStore from "../../stores/ui";
-import {Scene, Source} from "../../stores/scenes";
-import * as styles from "../../../styles/sources.scss";
+import {Source} from "../../stores/scenes";
+import {List, ListItem} from "./List";
 
 interface SourcesProps
 {
-	scenesStore: ScenesStore;
 	uiStore: UiStore;
 }
 
 @observer
 export default class Sources extends React.Component<SourcesProps, any>
 {
-	@computed
-	get currentScene(): Scene
-	{
-		return this.props.scenesStore.scenes
-			.find(s => s.name == this.props.uiStore.currentScene) as Scene;
-	}
-
-	onSceneChange(scene: Scene)
-	{
-		this.props.uiStore.currentScene = scene.name;
+	addSource = () => {
+		const {currentScene} = this.props.uiStore;
+		const source = new Source();
+		source.name = "Source " + (currentScene.sources.length + 1);
+		currentScene.addSource(source);
 	}
 
 	render()
 	{
+		const {currentScene} = this.props.uiStore;
 		return (
-			<div>
-				<p>Sources</p>
-				<ul>
-					{this.currentScene.sources.map(source =>
-						<SourceItem source={source} />)}
-				</ul>
+			<div style={{flex: 1}}>
+				<p>{currentScene.name} - {t("sources.listTitle")}</p>
+				<button onClick={this.addSource}>Add Source</button>
+				<List>
+					{currentScene.sources.map(s =>
+						<ListItem key={s.name} name={s.name} />)}
+				</List>
 			</div>
-		);
-	}
-}
-
-interface SourceItemProps
-{
-	source: Source
-}
-
-@observer
-class SourceItem extends React.Component<SourceItemProps, any>
-{
-	render()
-	{
-		return (
-			<li>Item</li>
 		);
 	}
 }
